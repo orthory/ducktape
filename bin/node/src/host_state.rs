2cc8a1da74eca69ed70fc6b0b0ddbdd53b9e1d1a
byeongsu-hong <frostornge@gmail.com>
2026-09-09 10:58:17 +0000

    build(genesis): pin roots for epoch 7 owner views

--- bin/node/src/host_state.rs
+++ bin/node/src/host_state.rs
@@ -891,7 +891,7 @@ mod tests {
     /// accident. Update it ONLY as the deliberate half of a flag day (see
     /// [`production_genesis_root_hash_is_pinned`]).
     const GENESIS_ROOT_HASH: &str =
-        "a357f1ee4f07fc0b5c97082ba5528929728255699a28c0bfeef5533ff9676d66";
+        "dc29069707c08ca0a555647fac887cf5fe0e128889dba64f071d47dcde71ea1b";
 
     /// The bindings [`GENESIS_ROOT_HASH`] is taken over. They are constants
     /// because they are NOT: each rides its module's genesis `__config`
